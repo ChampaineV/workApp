6d38dba50fb04241906379f3a5f1fc3ec32b49c5
Leischow Vang <champainevaj@gmail.com>
2023-04-04 17:26:12 -0500

    Add comments to TaskList class to methods

--- src/main/java/com/workapp/entity/TaskList.java
+++ src/main/java/com/workapp/entity/TaskList.java
@@ -38,50 +38,99 @@ public class TaskList {
         this.dateCreated = dateCreated;
     }
 
+    /**
+     * Gets task list id
+     *
+     * @return task list id
+     */
     public int getTaskListId() {
         return taskListId;
     }
 
+    /**
+     * Sets task list id
+     * @param taskListId task list id
+     */
     public void setTaskListId(int taskListId) {
         this.taskListId = taskListId;
     }
 
+    /**
+     * Gets the name of the task list
+     * @return name of the task list
+     */
     public String getTaskListName() {
         return taskListName;
     }
 
+    /**
+     * Sets the name of the task list
+     * @param taskListName name of the task list
+     */
     public void setTaskListName(String taskListName) {
         this.taskListName = taskListName;
     }
 
+    /**
+     * Gets the description of the task list
+     * @return the description of the task list
+     */
     public String getTaskListDescription() {
         return taskListDescription;
     }
 
+    /**
+     * Sets the description of the task list
+     * @param taskListDescription the description of the task list
+     */
     public void setTaskListDescription(String taskListDescription) {
         this.taskListDescription = taskListDescription;
     }
 
+    /**
+     * Gets the folder id
+     * @return folder id
+     */
     public int getFolderId() {
         return folderId;
     }
 
+    /**
+     * Sets the folder id
+     * @param folderId the folder id
+     */
     public void setFolderId(int folderId) {
         this.folderId = folderId;
     }
 
+    /**
+     * Gets the status of the task list.
+     * @return the status of the task list
+     */
     public boolean isCompleted() {
         return isCompleted;
     }
 
+    /**
+     * Sets the task list's status to completed or not
+     * @param completed the task list's status
+     */
     public void setCompleted(boolean completed) {
         isCompleted = completed;
     }
 
+    /**
+     * Gets the date the task list was created
+     * @return the date the task list was created
+     */
     public String getDateCreated() {
         return dateCreated;
     }
 
+    /**
+     * Sets the date the task list was created
+     * @param dateCreated the date the task list was created
+     */
     public void setDateCreated(String dateCreated) {
         this.dateCreated = dateCreated;
     }
